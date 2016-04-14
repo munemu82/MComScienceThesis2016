1b9877c75d922df06ced2a54353961b7da579842
amosAdmin <amosAdmin>
2016-04-15 07:29:16 +1000

    Added PhoG feature descriptor for testing

---
+++ phog/myownPhogExtract.m
@@ -0,0 +1,2 @@
+%phog feature vector was part of the below paper
+%A. Bosch, A. Zisserman & X. Munoz. 2007. Representing shape with a spatial pyramid kernel. In Proceedings of CIVR ’07 — [DOI] [PDF]
